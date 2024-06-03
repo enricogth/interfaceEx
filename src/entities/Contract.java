2d506b89f56e17c50257ce45766c324b9d20d40b
enricogth <enrigazoth@gmail.com>
2024-06-03 11:07:06 -0300

    Adding services part 1

--- src/entities/Contract.java
+++ src/entities/Contract.java
@@ -1,18 +1,19 @@
 package entities;
 
+import java.time.LocalDate;
+
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class Contract {
 
 	private Integer number;
-	private Date date;
+	private LocalDate date;
 	private Double totalValue;
 	
 	private List<Installment> installments = new ArrayList<>();
 	
-	public Contract(Integer number, Date date, Double totalValue) {
+	public Contract(Integer number, LocalDate date, Double totalValue) {
 		this.number = number;
 		this.date = date;
 		this.totalValue = totalValue;
@@ -29,12 +30,12 @@ public class Contract {
 	}
 
 
-	public Date getDate() {
+	public LocalDate getDate() {
 		return date;
 	}
 
 
-	public void setDate(Date date) {
+	public void setDate(LocalDate date) {
 		this.date = date;
 	}
 
